56ef92fc87b4871951ad5a0f962d5bc070ec252e
Darren Stallard <stalliardo187@gmail.com>
2024-02-27 16:19:58 +0000

    WIP

--- client/src/components/awards/AwardsForm.js
+++ client/src/components/awards/AwardsForm.js
@@ -9,7 +9,7 @@ const AwardsForm = () => {
     const [showAddRow, setShowAddRow] = useState(false);
 
     const handleChange = (e) => {
-
+        // TODO
     }
 
     return (
@@ -122,34 +122,34 @@ const AwardsForm = () => {
                         </tr>
 
                         {
-                            showAddRow && 
+                            showAddRow &&
                             <tr className='conditional-row'>
-                            <td>
-                                <input type='text' name='contractNumber' placeholder='Contract No' onChange={handleChange}/>
-                            </td>
-                            <td>
-                                <input type='text' name='contractNumber' placeholder='Project' onChange={handleChange}/>
-                            </td>
-                            <td>
-                                <input type='text' name='contractNumber' placeholder='Programme' onChange={handleChange}/>
-                            </td>
-                            <td>
-                                <input type='text' name='contractNumber' placeholder='Contractor' onChange={handleChange}/>
-                            </td>
-                            <td>
-                                <input type='text' name='contractNumber' placeholder='Region' onChange={handleChange}/>
-                            </td>
-                            <td>
-                                <input type='text' name='contractNumber' placeholder='Core' onChange={handleChange}/>
-                            </td>
-                            <td className='table-actions-cell' onClick={() => setShowAddRow(false)}>
-                                <button className='table-actions-cell edit'>Cancel</button>
-                            </td>
-                            <td className='table-actions-cell'>
-                                <button className='table-actions-cell edit'>Save</button>
-                            </td>
-                            
-                        </tr>
+                                <td>
+                                    <input type='text' name='contractNumber' placeholder='Contract No' onChange={handleChange} />
+                                </td>
+                                <td>
+                                    <input type='text' name='contractNumber' placeholder='Project' onChange={handleChange} />
+                                </td>
+                                <td>
+                                    <input type='text' name='contractNumber' placeholder='Programme' onChange={handleChange} />
+                                </td>
+                                <td>
+                                    <input type='text' name='contractNumber' placeholder='Contractor' onChange={handleChange} />
+                                </td>
+                                <td>
+                                    <input type='text' name='contractNumber' placeholder='Region' onChange={handleChange} />
+                                </td>
+                                <td>
+                                    <input type='text' name='contractNumber' placeholder='Core' onChange={handleChange} />
+                                </td>
+                                <td className='table-actions-cell' onClick={() => setShowAddRow(false)}>
+                                    <button className='table-actions-cell edit'>Cancel</button>
+                                </td>
+                                <td className='table-actions-cell'>
+                                    <button className='table-actions-cell edit'>Save</button>
+                                </td>
+
+                            </tr>
                         }
                     </tbody>
 

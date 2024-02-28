a89bdf919950a3fe343bab3ccf0864594efdcfdb
Darren Stallard <stalliardo187@gmail.com>
2024-02-28 12:12:06 +0000

    Navbar updates

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <nav className='navbar-container'>
         <div className='nav-end-container'>
-            <a href='/awards-totals'>Awards Summary</a>
             <a href='/awards-form'>Awards Form - Single</a>
             <a href='/awards-form-multi'>Awards Form - Mutli</a>
             <a href='/awards-form-select'>Awards Form - Select</a>
+            <a href='/awards-totals'>Awards Summary</a>
         </div>
     </nav>
   )

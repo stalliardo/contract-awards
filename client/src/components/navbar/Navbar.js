630167d54dd04879506af2170d830ef43331edbf
Darren Stallard <stalliardo187@gmail.com>
2024-03-26 13:14:49 +0000

    Huge work aound to get the redux state to persist during page transitions.

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './navbar.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   const handleLogOut = () => {
     // TODO
     console.log('log out clicked');
@@ -13,22 +11,10 @@ const Navbar = () => {
   return (
     <nav className='navbar-container'>
       <div className='nav-end-container'>
-        <a href='/' onClick={handleLogOut}>Sign Out</a>
-        <a href='/admin'>Admin</a>
-        <a href='/awards-form'>Awards</a>
-        <a href='/awards-totals'>Awards Summary</a>
-        <a href='/tenders-submitted'>Tenders Submitted</a>
-{/* 
-        <Router>
         <Link to="/">Sign Out</Link>
+        <Link to="/">Admin</Link>
         <Link to="/awards-form">Awards</Link>
-        
-        <Link to="/">Sign Out</Link>
-        </Router>
-
-        <Router>
-        <Link to="/awards-totals">Awards Summary</Link>
-        </Router> */}
+        <Link to="/awards-summary">Awards Summary</Link>
       </div>
     </nav>
   )
--- client/src/index.js
+++ client/src/index.js
@@ -4,76 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './routes/root';
-import Auth from './components/auth/Auth';
-import AwardsSummary from './components/awards/AwardsSummary';
-import Navbar from './components/navbar/Navbar';
-import DevPage from './components/developer/DevPage';
-import TendersSubmitted from './routes/TendersSubmitted';
-import Admin from './routes/Admin';
-
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './redux/reducers/';
 import { Provider } from 'react-redux';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/auth",
-    element: <Auth />
-  },
-  {
-    path: "/awards-form", // <- TODO
-    element: <Root />
-  },
-  {
-    path: "/tenders-submitted", // <- TODO
-    element: <TendersSubmitted />
-  },
-  {
-    path: "/admin", // <- TODO
-    element: <Admin />
-  },
-  {
-    path: "/awards-totals", // <- TODO
-    element: <AwardsSummary />
-  },
-  {
-    path: "/dev", // <- TODO
-    element: <DevPage />
-  }
-]);
 
 const store = configureStore({
   reducer: rootReducer
 })
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* Is the below line okay to go here? */}
-      <Navbar />
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

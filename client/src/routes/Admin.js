b3f6b3e64063e1958e3ec569bba4c68e450b5838
Darren Stallard <stalliardo187@gmail.com>
2024-04-05 12:40:24 +0100

    Admin page - updated notes

--- client/src/routes/Admin.js
+++ client/src/routes/Admin.js
@@ -104,19 +104,19 @@ export default Admin;
   // might also need to add a flag to the schema to differentiate between actual locations and special / MandE types
 
 
-  // Need the ability to add / remove locations? 
-  // Definietly will need the ability to add a location
-  // will probably be better to have a database model to handle these things
-  // Currently the locations const is static so if more places are added someone will have to manually add locations in multiple files and then rebuild the app etc...
+  // Need the ability to add / remove locations? - DONE
+  // Definietly will need the ability to add a location - DONE
+  // will probably be better to have a database model to handle these things - DONE
+  // Currently the locations const is static so if more places are added someone will have to manually add locations in multiple files and then rebuild the app etc... - DONE
 
 
-  // 1 - Get the current locations
-  // 2 - Add new location form
-  // 3 - how will adding new members be handled?
-  // 4 - What about what locations a user has access to??
+  // 1 - Get the current locations - DONE
+  // 2 - Add new location form - DONE
+  // 3 - how will adding new members be handled? - DONE
+  // 4 - What about what locations a user has access to?? - DONE
 
-  // /location/add-item
-  // /location/get-locations
+  // /location/add-item - DONE
+  // /location/get-locations - DONE
 
 
   // User roles via AD groups
@@ -129,19 +129,20 @@ export default Admin;
       // 3 - When viewing awards diaries
 
   // Users in state:
-    // When the app loads get all the members from the LDAP api's
-    // Filter / build the members in the db. Check the length of mebers returned from LDAP API's against the result from the db. if different either add or remove only the differnt user?
-    // Once rebuilt load the app
-    // Set the memebers data into state to be used across the app
-    // Then in the admin page the table will be populated by the memeber array in the store
-    // Will also need a way to determine who is the logged in user -> serparte Authentication feature and state object "auth"
-    // updating the values in the admin page will update the values in the database -> wont effect the LDAP groups
+    // When the app loads get all the members from the LDAP api's - DONE
+    // Filter / build the members in the db. Check the length of mebers returned from LDAP API's against the result from the db. if different either add or remove only the differnt user? - DONE
+    // Once rebuilt load the app - ????
+    // Set the memebers data into state to be used across the app - DONE
+    // Then in the admin page the table will be populated by the memeber array in the store - DONE
+    // Will also need a way to determine who is the logged in user -> serparte Authentication feature and state object "auth"               TODO
+    // updating the values in the admin page will update the values in the database -> wont effect the LDAP groups - DONE
 
   // Locations for the members in the users table
-    // If all display "All" or display the location count
-    // When cell clicked display a dropdown
-    // Needs to be editable
+    // If all display "All" or display the location count - DONE
+    // When cell clicked display a dropdown - DONE
+    // Needs to be editable - DONE
     
+  // Ability for Admins / and certain users to be able to add / edit the yearly totals for the summary page etc.
 
 
       

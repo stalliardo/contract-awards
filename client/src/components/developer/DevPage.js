bf40fb00469c874ac789016f213b0aa72c88a79b
Darren Stallard <stalliardo187@gmail.com>
2024-04-24 16:41:20 +0100

    Rebuilt the MONSTER of a function updateADUsers

--- client/src/components/developer/DevPage.js
+++ client/src/components/developer/DevPage.js
@@ -10,24 +10,15 @@ const functionCardData = [
         buttonText: "Generate Default Data"
     },
     {
-        title: "Generate all data for new financial year",
-        description: "This function is only to be called once at the start of the financial year.",
-        buttonText: "Generate Default Data"
+        title: "Update AD users",
+        description: "If a user has been added or removed from one of the CA groups use this function. This is required so the app doesnt have to perform unnecessary computations every time a user enters the app.",
+        buttonText: "Update AD Users"
     },
+
     {
-        title: "Generate all data for new financial year",
-        description: "This function is only to be called once at the start of the financial year. It will generate all the default data so the app can render tables with default values.",
-        buttonText: "Generate Default Data"
-    },
-    {
-        title: "Generate all data for new financial year",
-        description: "This function is only to be called once at the start of the financial year. It will generate all the default data so the app can render tables with default values.",
-        buttonText: "Generate Default Data"
-    },
-    {
-        title: "Generate all data for new financial year",
-        description: "This function is only to be called once at the start of the financial year. It will generate all the default data so the app can render tables with default values.",
-        buttonText: "Generate Default Data"
+        title: "Reassign a users group",
+        description: "If a users CA group has changed in active directoy use this function. This is required so the app doesnt have to perform unnecessary computations every time a user enters the app.",
+        buttonText: "Reassign User Group"
     },
 ]
 
@@ -36,9 +27,13 @@ const confirmationcallback = (text) => {
     return confirmation;
 }
 
-const DevFunctionCard = ({ title, description, buttonText, clickHandler, isLoading, loadingText }) => {
+const DevFunctionCard = ({ title, description, buttonText, clickHandler, isLoading, loadingText, requiresConfirmation = true }) => {
     const handleClick = () => {
-        if (confirmationcallback(`Are you sure you want to call the "${buttonText}" function?`)) {
+        if (requiresConfirmation) {
+            if (confirmationcallback(`Are you sure you want to call the "${buttonText}" function?`)) {
+                clickHandler();
+            }
+        } else {
             clickHandler();
         }
     }
@@ -46,7 +41,7 @@ const DevFunctionCard = ({ title, description, buttonText, clickHandler, isLoadi
     return (
         <div className='dev-function-card'>
             {
-                isLoading ? <div className='dev-spinner'><Spinner/></div> :
+                isLoading ? <div className='dev-spinner'><Spinner /></div> :
                     <>
                         <h3>{title}</h3>
                         <p>{description}</p>
@@ -61,19 +56,18 @@ const DevFunctionCard = ({ title, description, buttonText, clickHandler, isLoadi
 
 const DevPage = () => {
     const [isGeneratingAllData, setIsGeneratingAllData] = useState(false);
+    const [isUpdatingUsers, setIsUpdatingUsers] = useState(false);
 
     const generateAllDataForYear = async () => {
-        console.log('called');
+
+
         setIsGeneratingAllData(true);
-
-
         try {
             // generate all awards data...
             await axios.get("/api/awards-diary/generateAllData");
 
             // generate all tenders data...
             await axios.post("/api/tenders/generate-initial-data");
-
         } catch (error) {
             console.log('Error from gnenerateAllData: ', error);
         } finally {
@@ -83,19 +77,29 @@ const DevPage = () => {
         }
     }
 
-
-    const onGenerateTendersSubmittedData = () => {
-        axios.post("/api/tenders/generate-initial-data");
-    }
-
     const onGenerateDataForNewLocation = () => {
         axios.post("/api/tenders/generate-data-for-new-location", { location: "Bristol" });
     }
 
+    const onReassignUserClicked = () => {
+        console.log('reassign clicked');
 
+        // TODO is this required? This can be achieved using mongo compass software
+        // Future feature
+    }
 
-    const onclickHanlder = () => {
-        console.log('hankder called');
+    const onUpdateADUsers = async () => {
+        setIsUpdatingUsers(true);
+    
+        try {
+            await axios.get("/api/ad/update-AD-users");
+        } catch (error) {
+            console.log('error upading = ', error);
+        } finally { 
+            setTimeout(() => {
+                setIsUpdatingUsers(false);
+            }, 1250);
+        }
     }
 
 
@@ -104,32 +108,26 @@ const DevPage = () => {
             <h2>Site Administrator Functions:</h2>
             <p>Authorized personnel only. Misuse can result in application errors and data loss.</p>
             <div className='dev-functions-container'>
-                <DevFunctionCard title={functionCardData[0].title} 
-                    description={functionCardData[0].description} 
-                    buttonText={functionCardData[0].buttonText} 
-                    clickHandler={generateAllDataForYear} 
+                <DevFunctionCard title={functionCardData[0].title}
+                    description={functionCardData[0].description}
+                    buttonText={functionCardData[0].buttonText}
+                    clickHandler={generateAllDataForYear}
                     isLoading={isGeneratingAllData}
-                    loadingText="Generating Data..."
                 />
-                <DevFunctionCard title={functionCardData[1].title} description={functionCardData[1].description} buttonText={functionCardData[1].buttonText} clickHandler={onclickHanlder} />
-                <DevFunctionCard title={functionCardData[2].title} description={functionCardData[2].description} buttonText={functionCardData[2].buttonText} clickHandler={onclickHanlder} />
-                {/* {
-                    functionCardData.map((cardData, index) => {
-                        return (
-                        )
-                    })
-                } */}
-                {/* <div className='dev-awards-functions-container'>
-                    <h3>Awards functions</h3>
-                    
-                    <button onClick={generateAllDataForYear}>generateAllDataForYear</button>
-                </div>
-                <div className='dev-awards-functions-container'>
-                <h3>Admin functions</h3>
-                
-                <button onClick={onGenerateTendersSubmittedData}>Generate Tenders Submitted Data</button>
-                <button onClick={onGenerateDataForNewLocation}>Generate Tender for new location</button>
-                </div> */}
+                <DevFunctionCard
+                    title={functionCardData[1].title}
+                    description={functionCardData[1].description}
+                    buttonText={functionCardData[1].buttonText}
+                    isLoading={isUpdatingUsers}
+                    clickHandler={onUpdateADUsers}
+                />
+                {/* <DevFunctionCard
+                    title={functionCardData[2].title}
+                    description={functionCardData[2].description}
+                    buttonText={functionCardData[2].buttonText}
+                    clickHandler={onUpdateADUsers}
+                    requiresConfirmation={false}
+                /> */}
             </div>
         </div>
     )
@@ -142,3 +140,8 @@ export default DevPage;
 // Add or Remove users via their AD group
 // Change a users group / role. eg, they are moved from one folder in AD to another. This can be done using the mongodb compass software also
 // When a director adds a new location the default tenders data also needs to be updated using the "generate-data-for-new-location" endpoint
+
+
+// NEXT function:
+// edit users folders ie when moved from CA01 etc
+// simple function that allows an admin to select a user from a drowdown, then, select a new group and save.

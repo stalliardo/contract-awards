c5ae52d6a33984b618322d79747b2b6318c29712
Darren Stallard <stalliardo187@gmail.com>
2024-04-25 12:40:13 +0100

    File clean up

--- server/controllers/ADController.js
+++ server/controllers/ADController.js
@@ -42,7 +42,6 @@ exports.updateADUsers = async (req, res) => {
                                 itemToAdd.operation = "Update"
                             }
                         }
-
                         if(!matchingUser) {
                             // new user to be added
                             itemToAdd.operation = "Add";
@@ -60,7 +59,6 @@ exports.updateADUsers = async (req, res) => {
         } else {
             return res.status(404).send({ message: "No users found in active directory" });
         }
-
         const extractedUserNames = [];
 
         if(formattedData.length) {
@@ -108,7 +106,6 @@ exports.updateADUsers = async (req, res) => {
             }
         }
 
-        console.log('bottom called');
         return res.status(200).send({message: "Successfully updated users!"});
     } catch (error) {
         return res.status(500).send({ error: "An error occured. Error: " + error });
@@ -156,30 +153,3 @@ exports.retrieveUsersForGroup = async (req, res) => {
         return res.status(500).send({ message: "An error occured" });
     }
 }
-
-
-// Scenarios: 
-    // A user is moved from one folder to another CA03(user level) => CA02(RD level)
-    // Issue: the app will have idea that the change has taken place so will still think the user has the previous access level
-    // Handling Options: 
-        // A - Will have to change their folder access in Active Directory anyway so do we jump in the app and call a function?
-        // B - The app runs the large updateADUsers function above everytime a user enters the app?
-
-        // Option A:
-            // Will need the hidden site admin section / route to hold the ui and logic for this
-            // Functions for adding a user, removing a user and moving a user required. although the updateADUsers function handles adding and removing but needs testing
-
-
-    // TODO's 
-        // Make sure these endpoints use the token check middleware
-
-
-
-    // if(userLengthFromAD !== mongoUsers.length) <- noty sufficient
-    // better mothod required that checks for discrepencies between the ad data returned and the data returned from mongo
-    // For each user in mongo check their role then...
-        // check if a user with that name also is in the ad data returned
-        // if there is a match but the roles differ                                                             ****************** UPDATE THAT USER *********************
-        // if the user has is not in the data retuened from ad then have been remoed from the group             ****************** DELETE THAT USER *********************
-        // if the user is found but the role is the same do nothing
-        // will still first need toi check if a user has been added ie adlength > mongoUsers

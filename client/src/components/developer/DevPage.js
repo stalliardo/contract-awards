35da74413db2b3d1d519dc98ead09eb9870800a3
Darren Stallard <stalliardo187@gmail.com>
2024-04-25 08:48:14 +0100

    Added the site-admin functions

--- client/src/components/developer/DevPage.js
+++ client/src/components/developer/DevPage.js
@@ -14,12 +14,6 @@ const functionCardData = [
         description: "If a user has been added or removed from one of the CA groups use this function. This is required so the app doesnt have to perform unnecessary computations every time a user enters the app.",
         buttonText: "Update AD Users"
     },
-
-    {
-        title: "Reassign a users group",
-        description: "If a users CA group has changed in active directoy use this function. This is required so the app doesnt have to perform unnecessary computations every time a user enters the app.",
-        buttonText: "Reassign User Group"
-    },
 ]
 
 const confirmationcallback = (text) => {
@@ -59,8 +53,6 @@ const DevPage = () => {
     const [isUpdatingUsers, setIsUpdatingUsers] = useState(false);
 
     const generateAllDataForYear = async () => {
-
-
         setIsGeneratingAllData(true);
         try {
             // generate all awards data...
@@ -77,17 +69,6 @@ const DevPage = () => {
         }
     }
 
-    const onGenerateDataForNewLocation = () => {
-        axios.post("/api/tenders/generate-data-for-new-location", { location: "Bristol" });
-    }
-
-    const onReassignUserClicked = () => {
-        console.log('reassign clicked');
-
-        // TODO is this required? This can be achieved using mongo compass software
-        // Future feature
-    }
-
     const onUpdateADUsers = async () => {
         setIsUpdatingUsers(true);
     
@@ -101,7 +82,6 @@ const DevPage = () => {
             }, 1250);
         }
     }
-
 
     return (
         <div className='dev-page-container'>
@@ -121,27 +101,8 @@ const DevPage = () => {
                     isLoading={isUpdatingUsers}
                     clickHandler={onUpdateADUsers}
                 />
-                {/* <DevFunctionCard
-                    title={functionCardData[2].title}
-                    description={functionCardData[2].description}
-                    buttonText={functionCardData[2].buttonText}
-                    clickHandler={onUpdateADUsers}
-                    requiresConfirmation={false}
-                /> */}
             </div>
         </div>
     )
 }
 export default DevPage;
-
-// functions required for site-admins
-// The ability to set the database name?
-// Create the default data for the year ie awards and tenders
-// Add or Remove users via their AD group
-// Change a users group / role. eg, they are moved from one folder in AD to another. This can be done using the mongodb compass software also
-// When a director adds a new location the default tenders data also needs to be updated using the "generate-data-for-new-location" endpoint
-
-
-// NEXT function:
-// edit users folders ie when moved from CA01 etc
-// simple function that allows an admin to select a user from a drowdown, then, select a new group and save.

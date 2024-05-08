a23a3135dc11e4a77e1d29f1ca93e771ececfce5
Darren Stallard <stalliardo187@gmail.com>
2024-05-08 15:03:20 +0100

    Removed the placeholder from the select menu options

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -45,29 +45,22 @@ const Navbar = () => {
 
   const onFinancialYearSelected = (year) => {
 
-    // dispatch(setSelectedFinancialYear(removeSlashFromyearString(year.value)));
-
-    // console.log('year = ', year);
-
-    // display a modal
     setselectedYear(year);
     setShowWarningModal(true);
-    
   }
-
+  
   const onProceed = () => {
-    console.log('selected = ', selectedYear);
     setShowWarningModal(false);
     dispatch(setSelectedFinancialYear(removeSlashFromyearString(selectedYear.value)));
     dispatch(resetState());
     dispatch(resetTenderState());
 
     navigate("/admin");
+  }
 
-
-
-    // states cleared, now load new data based on the selectedyear
-  
+  const onClose = () => {
+    setSelectedFinancialYear()
+    setShowWarningModal(false)
   }
 
   return (
@@ -80,11 +73,10 @@ const Navbar = () => {
               {
                 authenticatedUser.role === ROLES.CA01 &&
                 <div className='navbar-select-container'>
-                  {/* Select menu should only be visible when the route is /admin */}
                   {
                     showSelectMenu &&
                     <>  
-                      <SelectMenu placeholder={addSlashToYearString(selectedFinancialYear)} menuItems={menuItems} handleItemSelection={onFinancialYearSelected} />
+                      <SelectMenu allSettingPlaceholder={false} placeholder={addSlashToYearString(selectedFinancialYear)} menuItems={menuItems} handleItemSelection={onFinancialYearSelected} />
                     </>
                   }
                 </div>
@@ -98,9 +90,6 @@ const Navbar = () => {
               <Link to="/awards-form">Awards</Link>
               <Link to="/awards-summary">Awards Summary</Link>
               <Link to="/tenders-submitted">Tenders Submitted</Link>
-
-              
-
               <a onClick={handleSignOut}>Sign Out</a>
             </>
             :
@@ -119,47 +108,10 @@ const Navbar = () => {
 
       {
         showWarningModal &&
-        <YearChangeWarningModal onClose={() => setShowWarningModal(false)} onProceed={onProceed}/>
+        <YearChangeWarningModal onClose={onClose} onProceed={onProceed}/>
       }
     </nav>
   )
 }
 
 export default Navbar;
-
-// TODOs
-  // - Select menu should only be available on the admin page?
-  // - Select menu onClick will display a notification modal
-
-// Will Load the current year by default
-// The selectmenu options will contain all options from 23/24 so 23/24, 24/25, 25/26 etc
-// So can set the placeholder as the latest year returned from the generateFinancialYearOptions function
-// When a user selects an older year need to:
-  // 1 - Display a modal that warns the users that they are about to view historic data and the data cannot be edited. etc
-  // 2 - When the user clicks proceed:
-    // A - Will get the data via financialYear
-    // B - Set the financialYear in state so any page changes where data is reloaded loads the correct data
-    // C - Will need to detect if in edit or readOnly mode and disbable certain actions if in readonly mode
-    // D - Will need to format the string 23/24 to 2324 so this can be used to filter by financial year - So when user clicks Select menu item - convert using the removeSlashFromyearString this can then be used to query the year in the db
-
-
-// Stage 1 - Current year, adding to state and passing year tom fetch api calls:
-  // - App Loads, user is authed, then user data is fetched from fetchUsers
-  // When loaded the AwardsTable loads the locaiton data via let url = `/api/awards-diary/location?location=${encodedLocation}`
-  // When entering awardsSummary route: the data is fetcvhed from two locations
-    // 1 - for the awardsData: /api/awards-diary/getAllAwards
-    // 2 - for the targets data: /api/targets
-  // All that needs to happen is to pass the year "2324" along with any request that gets year dependant data.
-  // Frontend needs to know if the year selected from the navbar select memu is the current year or not then set the readOnly flag based on that.
-
-// Stage 2 - Check what edit operations require:
-  // When updating / editing an item, will the year be required?
-  // If updating / edinting then no ^, the year should already be set
-
-// Next issue is the locations. Say in year 2324 there are 6 locations and data is set to each of these nice
-  // Then, five years later there are now 20 locations again, fine, but.. what happends when a user loads the data from 5 years ago there will only be 5 locations saved but there will be 20 locations in the db....
-
-
-  // And what if an old location no longer exists? It wont be displayed in the table at all
-
-  // Disable warning when a user slecets the current year from the selectmenu

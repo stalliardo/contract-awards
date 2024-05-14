843f96402b57c93f22a905c89b3bab3b9aafb92c
Darren Stallard <stalliardo187@gmail.com>
2024-05-14 16:43:51 +0100

    Added authentication to thje mongo database and added to connectionUrl

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 // MongoDB connection string, will need to be dynamic so financial years can be assigned to database names
-const connectionString = 'mongodb://192.168.122.97:27017/ContractAwards';
+// const connectionString = 'mongodb://192.168.122.97:27017/ContractAwards';
+
+const connectionString = `mongodb://darren:${process.env.MONGO_PASSWORD}@192.168.122.50:27017/database2324`;
 
 // Connect to MongoDB
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })

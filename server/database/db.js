1e2eaa983a001b836a5e06af9fbbe529f487e4a2
Darren Stallard <stalliardo187@gmail.com>
2024-05-14 12:07:37 +0100

    Changed the connection URL

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // MongoDB connection string, will need to be dynamic so financial years can be assigned to database names
-const connectionString = 'mongodb://192.168.122.50:27017/database2324';
+const connectionString = 'mongodb://192.168.122.97:27017/ContractAwards';
 
 // Connect to MongoDB
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
